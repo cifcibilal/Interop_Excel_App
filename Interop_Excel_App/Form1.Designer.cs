f7ffe2e6b85ebd767768d6b5d890045c570604ab
cifcibilal <cifci.bilal@outlook.com>
2023-08-31 17:00:37 +0300

    Proje dosyası ekle.

---
+++ Interop_Excel_App/Form1.Designer.cs
@@ -0,0 +1,231 @@
+﻿namespace Interop_Excel_App
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.comboBoxSheetNames = new System.Windows.Forms.ComboBox();
+            this.labelDosyaDurumu = new System.Windows.Forms.Label();
+            this.btnFileOpen = new System.Windows.Forms.Button();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.btnFilterAge = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.textBoxSaveSheetName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dataGridView1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(1239, 306);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1233, 285);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBoxSheetNames);
+            this.groupBox2.Controls.Add(this.labelDosyaDurumu);
+            this.groupBox2.Controls.Add(this.btnFileOpen);
+            this.groupBox2.Location = new System.Drawing.Point(15, 324);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(1233, 79);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(335, 34);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(73, 16);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sayfa seç :";
+            // 
+            // comboBoxSheetNames
+            // 
+            this.comboBoxSheetNames.FormattingEnabled = true;
+            this.comboBoxSheetNames.Location = new System.Drawing.Point(418, 31);
+            this.comboBoxSheetNames.Name = "comboBoxSheetNames";
+            this.comboBoxSheetNames.Size = new System.Drawing.Size(121, 24);
+            this.comboBoxSheetNames.TabIndex = 2;
+            this.comboBoxSheetNames.SelectedIndexChanged += new System.EventHandler(this.comboBoxSheetNames_SelectedIndexChanged);
+            // 
+            // labelDosyaDurumu
+            // 
+            this.labelDosyaDurumu.AutoSize = true;
+            this.labelDosyaDurumu.Location = new System.Drawing.Point(6, 34);
+            this.labelDosyaDurumu.Name = "labelDosyaDurumu";
+            this.labelDosyaDurumu.Size = new System.Drawing.Size(77, 16);
+            this.labelDosyaDurumu.TabIndex = 1;
+            this.labelDosyaDurumu.Text = "Beklemede";
+            // 
+            // btnFileOpen
+            // 
+            this.btnFileOpen.Location = new System.Drawing.Point(106, 21);
+            this.btnFileOpen.Name = "btnFileOpen";
+            this.btnFileOpen.Size = new System.Drawing.Size(145, 42);
+            this.btnFileOpen.TabIndex = 0;
+            this.btnFileOpen.Text = "Dosya Aç";
+            this.btnFileOpen.UseVisualStyleBackColor = true;
+            this.btnFileOpen.Click += new System.EventHandler(this.btnFileOpen_Click);
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.dataGridView2);
+            this.groupBox3.Location = new System.Drawing.Point(15, 409);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(1239, 219);
+            this.groupBox3.TabIndex = 1;
+            this.groupBox3.TabStop = false;
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 18);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.RowHeadersWidth = 51;
+            this.dataGridView2.RowTemplate.Height = 24;
+            this.dataGridView2.Size = new System.Drawing.Size(1233, 198);
+            this.dataGridView2.TabIndex = 0;
+            // 
+            // btnFilterAge
+            // 
+            this.btnFilterAge.Location = new System.Drawing.Point(24, 658);
+            this.btnFilterAge.Name = "btnFilterAge";
+            this.btnFilterAge.Size = new System.Drawing.Size(145, 42);
+            this.btnFilterAge.TabIndex = 2;
+            this.btnFilterAge.Text = "Filtrele";
+            this.btnFilterAge.UseVisualStyleBackColor = true;
+            this.btnFilterAge.Click += new System.EventHandler(this.btnFilterAge_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 639);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(66, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "25-35 Yaş";
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(546, 658);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(145, 42);
+            this.btnSave.TabIndex = 3;
+            this.btnSave.Text = "Kaydet";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // textBoxSaveSheetName
+            // 
+            this.textBoxSaveSheetName.Location = new System.Drawing.Point(318, 678);
+            this.textBoxSaveSheetName.Name = "textBoxSaveSheetName";
+            this.textBoxSaveSheetName.Size = new System.Drawing.Size(222, 22);
+            this.textBoxSaveSheetName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(315, 658);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(225, 16);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kaydediilecek Sayfa Adını Belirleyin:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1263, 722);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBoxSaveSheetName);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnFilterAge);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label labelDosyaDurumu;
+        private System.Windows.Forms.Button btnFileOpen;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.Button btnFilterAge;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.TextBox textBoxSaveSheetName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox comboBoxSheetNames;
+    }
+}
+
---
+++ Interop_Excel_App/Form1.cs
@@ -0,0 +1,215 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Data;
+using System.IO;
+using System.Windows.Forms;
+
+namespace Interop_Excel_App
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            ExcelPackage.LicenseContext = OfficeOpenXml.LicenseContext.NonCommercial;
+        }
+
+        private void btnFileOpen_Click(object sender, EventArgs e)
+        {
+
+            string filePath = DosyaAcVePathBul();
+            groupBox1.Text = filePath;
+            try
+            {
+
+                using (var package = new ExcelPackage(new FileInfo(filePath)))
+                {
+                    ExcelWorksheet worksheet = package.Workbook.Worksheets[0];
+                    DataTable dataTable = new DataTable();
+
+                    foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+                    {
+                        dataTable.Columns.Add(firstRowCell.Text);
+                    }
+
+                    for (var rowNumber = 2; rowNumber <= worksheet.Dimension.End.Row; rowNumber++)
+                    {
+                        var row = worksheet.Cells[rowNumber, 1, rowNumber, worksheet.Dimension.End.Column];
+                        var newRow = dataTable.NewRow();
+                        foreach (var cell in row)
+                        {
+                            newRow[cell.Start.Column - 1] = cell.Text;
+                        }
+                        dataTable.Rows.Add(newRow);
+                    }
+                    dataGridView1.DataSource = dataTable;
+                    labelDosyaDurumu.Text = "Açıldı";
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Hata oluştu:" + ex.Message, "Hata", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            comboBoxSheetNames.Items.Clear();
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                foreach (var sheet in package.Workbook.Worksheets)
+                {
+                    comboBoxSheetNames.Items.Add(sheet);
+                }
+            }
+            comboBoxSheetNames.SelectedIndex = 0;
+        }
+        private string DosyaAcVePathBul()
+        {
+            labelDosyaDurumu.Text = "Açılıyor...";
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            openFileDialog.Filter = "Excel Files|*.xlsx;*.xls";
+            openFileDialog.ShowDialog();
+            return openFileDialog.FileName;
+        }
+        private DataTable FilterByAge(DataTable originalTable, int minAge, int maxAge)
+        {
+            DataTable filteredTable = originalTable.Clone(); // Yeni bir tablo oluştur (sadece sütunlarını kopyala)
+
+            foreach (DataRow row in originalTable.Rows)
+            {
+                int age = Convert.ToInt32(row["Age"]); // "Age" sütunundaki yaş değerini al
+                if (age >= minAge && age <= maxAge)
+                {
+                    filteredTable.ImportRow(row); // Yaş aralığına uyan satırı kopyala
+                }
+            }
+
+            return filteredTable;
+        }
+        private void SaveFilteredDataToWorksheet(ExcelPackage package, DataTable filteredData,string sheetName)
+        {
+            ExcelWorksheet worksheet = package.Workbook.Worksheets.Add(sheetName); // Yeni bir sayfa oluştur
+
+            // Başlık satırını ekle
+            for (int col = 0; col < filteredData.Columns.Count; col++)
+            {
+                worksheet.Cells[1, col + 1].Value = filteredData.Columns[col].ColumnName;
+            }
+
+            // Verileri ekle
+            for (int row = 0; row < filteredData.Rows.Count; row++)
+            {
+                for (int col = 0; col < filteredData.Columns.Count; col++)
+                {
+                    worksheet.Cells[row + 2, col + 1].Value = filteredData.Rows[row][col];
+                }
+            }
+        }
+        private void ShowFilteredDataInDataGridView2(DataTable filteredData)
+        {
+            dataGridView2.DataSource = filteredData;
+        }
+
+        private void btnFilterAge_Click(object sender, EventArgs e)
+        {
+            int minAge = 25; // Minimum yaş
+            int maxAge = 35; // Maksimum yaş
+
+            DataTable originalData = (DataTable)dataGridView1.DataSource; // Verilerin olduğu kaynak
+
+            DataTable filteredData = FilterByAge(originalData, minAge, maxAge);
+
+            ShowFilteredDataInDataGridView2(filteredData);
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            int minAge = 25; // Minimum yaş
+            int maxAge = 35; // Maksimum yaş
+
+            string saveSheetName = textBoxSaveSheetName.Text;
+
+            if (string.IsNullOrEmpty(saveSheetName))
+            {
+                MessageBox.Show("Lütfen kaydetmek istediğiniz sayfa adını girin.", "Uyarı", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return; // İşlemi durdur
+            }
+
+            DataTable originalData = (DataTable)dataGridView1.DataSource; // Verilerin olduğu kaynak
+
+            DataTable filteredData = FilterByAge(originalData, minAge, maxAge);
+
+            string outputPath = @"C:\Users\muham\Desktop\Employee Sample Data.xlsx";
+
+            using (var package = new ExcelPackage(new FileInfo(outputPath)))
+            {
+                ExcelWorksheet existingWorksheet = package.Workbook.Worksheets[saveSheetName];
+                if (existingWorksheet != null)
+                {
+                    DialogResult result = MessageBox.Show("Girilen sayfa adında bir sayfa zaten var. Verileri üzerine yazmak istiyor musunuz?", "Uyarı", MessageBoxButtons.YesNo, MessageBoxIcon.Warning);
+                    if (result == DialogResult.Yes)
+                    {
+                        package.Workbook.Worksheets.Delete(existingWorksheet);
+                        SaveFilteredDataToWorksheet(package, filteredData, saveSheetName);
+
+                        package.Save();
+                        MessageBox.Show("Filtrelenmiş veriler yeni sayfaya kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        // Kullanıcı "Hayır" dediğinde hiçbir işlem yapma
+                        MessageBox.Show("Veriler üzerine yazma işlemi iptal edildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                }
+                else 
+                {
+
+                    SaveFilteredDataToWorksheet(package, filteredData, saveSheetName);
+                    package.Save();
+                    MessageBox.Show("Filtrelenmiş veriler yeni sayfaya kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                // Filtrelenmiş verileri yeni bir sayfaya kaydetme
+                //SaveFilteredDataToWorksheet(package, filteredData,saveSheetName);
+
+                //package.Save();
+            }
+
+            //MessageBox.Show("Filtrelenmiş veriler yeni sayfaya kaydedildi.", "Bilgi", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void comboBoxSheetNames_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string filePath = groupBox1.Text;
+            string selectedSheetName = comboBoxSheetNames.SelectedItem.ToString();
+
+
+
+            using (var package = new ExcelPackage(new FileInfo(filePath)))
+            {
+                ExcelWorksheet worksheet = package.Workbook.Worksheets[selectedSheetName];
+                if (worksheet != null)
+                {
+                    DataTable dataTable = new DataTable();
+
+                    foreach (var firstRowCell in worksheet.Cells[1, 1, 1, worksheet.Dimension.End.Column])
+                    {
+                        dataTable.Columns.Add(firstRowCell.Text);
+                    }
+
+                    for (var rowNumber = 2; rowNumber <= worksheet.Dimension.End.Row; rowNumber++)
+                    {
+                        var row = worksheet.Cells[rowNumber, 1, rowNumber, worksheet.Dimension.End.Column];
+                        var newRow = dataTable.NewRow();
+                        foreach (var cell in row)
+                        {
+                            newRow[cell.Start.Column - 1] = cell.Text;
+                        }
+                        dataTable.Rows.Add(newRow);
+                    }
+                    dataGridView1.DataSource = dataTable;
+                    labelDosyaDurumu.Text = "Açıldı";
+                }
+            }
+        }
+    }
+}
